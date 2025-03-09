c0119cccbf834bae3bf1cce67a58c905c7916a00
P4ST4S <antoine.rospars@epitech.eu>
2025-03-09 18:54:35 +0100

    feat(ingredients): add button logic and futur var

--- app/screens/ingredients.tsx
+++ app/screens/ingredients.tsx
@@ -1,6 +1,28 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
 
 export default function Ingredients() {
+  const [ingredientName, setIngredientName] = useState("");
+  const [ingredientCategory, setIngredientCategory] = useState("");
+  const [ingredientFreshness, setIngredientFreshness] = useState("");
+
+  const [isDrawerCategoriesOpen, setIsDrawerCategoriesOpen] = useState(false);
+  const [isDrawerFreshnessOpen, setIsDrawerFreshnessOpen] = useState(false);
+
+  const handleDrawerCategoriesPress = () => {
+    setIsDrawerCategoriesOpen(!isDrawerCategoriesOpen);
+  };
+
+  const handleDrawerFreshnessPress = () => {
+    setIsDrawerFreshnessOpen(!isDrawerFreshnessOpen);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Ingrédients</Text>
@@ -8,18 +30,28 @@ export default function Ingredients() {
       <View style={styles.inputIngredients}>
         <Text style={styles.inputIngredientsText}>Nommez votre ingrédient</Text>
       </View>
-      <View style={styles.drawerIngredientsCategories}>
-        <Text style={styles.drawerIngredientsCategoriesText}>{">"}</Text>
+      <TouchableOpacity
+        style={styles.drawerIngredientsCategories}
+        onPress={handleDrawerCategoriesPress}
+      >
+        <Text style={styles.drawerIngredientsCategoriesText}>
+          {isDrawerCategoriesOpen ? "V" : ">"}
+        </Text>
         <Text style={styles.drawerIngredientsCategoriesText}>
           Classe de l'ingrédient
         </Text>
-      </View>
-      <View style={styles.drawerIngredientsCategories}>
-        <Text style={styles.drawerIngredientsCategoriesText}>{">"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleDrawerFreshnessPress}
+        style={styles.drawerIngredientsCategories}
+      >
+        <Text style={styles.drawerIngredientsCategoriesText}>
+          {isDrawerFreshnessOpen ? "V" : ">"}
+        </Text>
         <Text style={styles.drawerIngredientsCategoriesText}>
           Fraîcheur de l'ingrédient
         </Text>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 }
